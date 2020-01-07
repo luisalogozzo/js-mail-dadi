3b61288c6f8be11ab21b7058f5dbf2ddeafbe88a
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-01-07 18:12:40 +0100

    Up

---
+++ dadi/js/script.js
@@ -0,0 +1,11 @@
+var NumeroCasuale1 = Math.floor(Math.random() * 7 + 1);
+document.getElementById('dado1').innerHTML = NumeroCasuale1
+var NumeroCasuale2 = Math.floor(Math.random() * 7 + 1);
+document.getElementById('dado2').innerHTML = NumeroCasuale2
+if (NumeroCasuale1 > NumeroCasuale2) {
+  document.getElementById('esito').innerHTML = 'Vince il dado a sinistra';
+} else if (NumeroCasuale2 > NumeroCasuale1) {
+  document.getElementById('esito').innerHTML = 'Vince il dado a destra';
+} else {
+  document.getElementById('esito').innerHTML = 'Pareggio';
+}
